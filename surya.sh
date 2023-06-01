07309764cf66325ba3476d900f3e093895d1ad94
Otávio Nóbrega <84592336+vesguin@users.noreply.github.com>
2023-06-01 12:02:44 -0300

    add surya.sh

---
+++ surya.sh
@@ -0,0 +1,28 @@
+#!/usr/bin
+
+VESGUIN="https://github.com/vesguin"
+LOS="https://github.com/LineageOs"
+
+# Device Tree
+"$(rm -rf device/xiaomi/surya)"
+git clone ${VESGUIN}/android_device_xiaomi_surya -b arrow device/xiaomi/surya;
+
+# Vendor Tree
+"$(rm -rf vendor/xiaomi/surya)"
+git clone ${VESGUIN}/android_vendor_xiaomi_surya -b arrow vendor/xiaomi/surya;
+
+# Kernel Tree
+"$(rm -rf kernel/xiaomi/surya)"
+git clone ${VESGUIN}/android_kernel_xiaomi_surya -b arrow kernel/xiaomi/surya;
+
+#HALS
+"$(rm -rf hardware/qcom-caf/sm8150/display)"
+git clone ${LOS}/android_hardware_qcom_display -b lineage-20.0-caf-sm8150 hardware/qcom-caf/sm8150/display;
+"$(rm -rf hardware/qcom-caf/sm8150/media)"
+git clone ${LOS}/android_hardware_qcom_media -b lineage-20.0-caf-sm8150 hardware/qcom-caf/sm8150/media;
+"$(rm -rf hardware/qcom-caf/sm8150/audio)"
+git clone ${LOS}/android_hardware_qcom_audio -b lineage-20.0-caf-sm8150 hardware/qcom-caf/sm8150/audio;
+
+#Dependencies
+"$(rm -rf hardware/xiaomi)"
+git clone ${LOS}/android_hardware_xiaomi -b lineage-20 hardware/xiaomi;
